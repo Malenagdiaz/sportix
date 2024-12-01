7742f3b07e8e0259e6a1450e5f03e0c1e378f913
Malena <mgerezdiaz@gmail.com>
2024-12-01 02:24:51 -0300

    Incorporacion de tema a mi pagina

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+import theme from "./theme/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
---
+++ src/theme/ThemeProvider.jsx
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+import "@fontsource/montserrat";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Montserrat", sans-serif',
+  },
+});
+
+export default theme;
